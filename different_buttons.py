a8ed8db52532bfef83ab13ddeb0dfb999e3d4e29
mbibin <bibinphmathew@gmail.com>
2022-07-22 22:23:25 +0530

    test buttons

---
+++ different_buttons.py
@@ -0,0 +1,27 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+class OpenHomePage:
+    def home_page(self):
+
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+
+        driver.get("http://www.leafground.com/home.html")
+        driver.maximize_window()
+        url = "pages/Button.html"
+        driver.find_element(By.XPATH, '//a[@href="'+url+'"]').click()
+        driver.find_element(By.ID, "home").click()
+        driver.back()
+        location = driver.find_element(By.ID, "position").location
+        print(location)
+        rgb = driver.find_element(By.ID, "color").value_of_css_property("background-color")
+        print(rgb)
+        size = driver.find_element(By.ID, "size").size
+        print(size)
+
+
+run = OpenHomePage()
+run.home_page()
